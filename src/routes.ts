855c2b5dfff4e2c062704daa8bb7420853d5e6ff
alvaro-ian <alvaro-ian@hotmail.com>
2020-08-26 17:56:53 -0300

    deleting encryption and decryption

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,11 @@
 import { Router } from 'express'
 
 import GenerationController from './controllers/GenerationController'
-import DecryptController from './controllers/DecryptController'
-import EncryptController from './controllers/EncryptController'
 import SignController from './controllers/SignController'
 
 const routes = Router()
 
 routes.get('/generation', GenerationController.new)
-routes.post('/encrypt', EncryptController.encrypt)
-routes.post('/decrypt', DecryptController.decrypt)
 routes.post('/sign', SignController.sign)
 routes.post('/verify', SignController.verify)
 
